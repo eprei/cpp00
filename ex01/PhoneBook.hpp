0bec8fe3283ec1d83ca20184174db9e4073357e6
Emiliano Presa custodio <epresa-c@c2r8s9.42lausanne.ch>
2022-11-10 16:47:13 +0100

    public-pripublic-privatevate

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -12,15 +12,16 @@ public:
 	void	start_phone(void);
 	bool	get_exit_status(void) const;
 	void	set_exit_status(int i);
-	void	ft_add_contact(void);
-	void	ft_search(void);
-	void	print_all_contacts(void) const;
 
 private:
 
 	std::string _prompt;
 	Contact _contact[8];
 	bool	_exit_status;
+
+	void	ft_add_contact(void);
+	void	ft_search(void);
+	void	print_all_contacts(void) const;
 };
 
 #endif
